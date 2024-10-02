51dac9539f48bb85d8054510749348b2134417ec
Praveen Redamalla <praveen.redamalla@acsicorp.com>
2024-10-02 19:23:24 +0530

    initial commit

---
+++ src/test/java/com/praveen/cicd/cicdpipelinr/CicdpipelinrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveen.cicd.cicdpipelinr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdpipelinrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
